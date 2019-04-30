cc9f5b3f02441c30d0fdc6affdad93014abb6cea
Codeweaver <jereco11@gmail.com>
2019-04-30 23:01:59 +0100

    Project Startup

---
+++ DenMarkTest.DataAccessLayer/Dbcontexts/DanishContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DenMarkTest.DataAccessLayer.Dbcontexts
+{
+    public class DanishContext:DbContext
+    {
+
+        public DanishContext(DbContextOptions<DanishContext> options)
+                  : base(options)
+        {
+            Database.Migrate();
+
+        }
+        public DbSet<string> companyProfile { get; set; }
+
+    }
+}
