b7e68d276e9d44095d3df85924b220313df2c011
Codeweaver <jereco11@gmail.com>
2019-05-01 00:12:42 +0100

    Added Generic repository

---
+++ DenMarkTest.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using DenMarkTest.DataAccessLayer.Dbcontexts;
+using DenMarkTest.DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenMarkTest.DataAccessLayer.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private DanishContext _dbContext;
+        public GenericRepository(DanishContext dbContect)
+        {
+            _dbContext = dbContect;
+        }
+
+        public async Task<TEntity> Create(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> Update(int id, TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var entity = await GetById(id);
+            var res = false;
+            _dbContext.Set<TEntity>().Remove(entity);
+            var result = await _dbContext.SaveChangesAsync();
+            if (result > 0)
+            {
+                res = true;
+            }
+            return res;
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbContext.Set<TEntity>().AsNoTracking();
+        }
+
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _dbContext.Set<TEntity>()
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync();
+        }
+    }
+}
