bcc9015a3686c4a94d84b5e5cb2f1ed31704b672
Codeweaver <jereco11@gmail.com>
2019-05-01 01:16:23 +0100

    Added project readme

--- DenMarkTest.core/Services/TestService.cs
+++ DenMarkTest.core/Services/TestService.cs
@@ -1,4 +1,5 @@
 ﻿using DenMarkTest.core.Abstract;
+using DenMarkTest.DataAccessLayer.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace DenMarkTest.core.Services
 {
-   public class TestService: ITestService
+    public class TestService : ITestService
     {
+        public Task<Test> createTest(string testType, DateTime testDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TestParticipants> getAllTest()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
