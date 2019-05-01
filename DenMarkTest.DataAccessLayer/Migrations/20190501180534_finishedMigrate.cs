e4c093d8217ce3680c4b3a7d25e3bf9e2178f639
Codeweaver <jereco11@gmail.com>
2019-05-01 19:06:57 +0100

    after migrations

---
+++ DenMarkTest.DataAccessLayer/Migrations/20190501180534_finishedMigrate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DenMarkTest.DataAccessLayer.Migrations
+{
+    public partial class finishedMigrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
